143fecc7f91f738ade503395015bee428e5c430e
Andrew <avashchenock@gmail.com>
2019-01-18 01:51:33 +0200

    barely workin 1 msg

--- src/socketClient.java
+++ src/socketClient.java
@@ -6,17 +6,18 @@ import javax.swing.JOptionPane;
 
 public class socketClient {
     public void run() throws IOException {
+        Socket s = new Socket(InetAddress.getLocalHost(), 9090);
         while (true) {
             String message = JOptionPane.showInputDialog(
                     "Enter your message to server:");
-            Socket s = new Socket(InetAddress.getLocalHost(), 9090);
             PrintWriter out =
                     new PrintWriter(s.getOutputStream(), true);
             out.println(message);
             if(JOptionPane.showConfirmDialog(null,
                     "One more message?", "One question...", JOptionPane.YES_NO_OPTION) == 1) {
+            }
+            else {
                 System.exit(0);
-                break;
             }
         }
     }
