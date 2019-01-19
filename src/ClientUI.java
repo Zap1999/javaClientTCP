dff0377b5d534c34d61611f5182542b304100717
Andrew <avashchenock@gmail.com>
2019-01-19 02:18:08 +0200

    sending and closing working

--- src/ClientUI.java
+++ src/ClientUI.java
@@ -1,4 +1,51 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ClientUI {
+    private JPanel mainPanel;
+    private JTextField msgField;
+    private JButton sendMessageButton;
+    private JButton endItButton;
+    private JButton connectButton;
+    static public ClientUI ui = new ClientUI();
+    socketClient clientSocket;
+
+    JTextField getMsgField(){
+        return msgField;
+    }
+
+    public ClientUI() {
+        connectButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                socketClient client1 = new socketClient(ui);
+                client1.run();
+                clientSocket = client1;
+            }
+        });
+        sendMessageButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                clientSocket.sendMessage();
+            }
+        });
+        endItButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                clientSocket.sendLastMsg();
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("ClientUI");
+        frame.setContentPane(ui.mainPanel);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+
+
 }
--- src/socketClient.java
+++ src/socketClient.java
@@ -2,23 +2,32 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.io.PrintWriter;
-import javax.swing.JOptionPane;
 
 public class socketClient {
-    public void run() throws IOException {
-        Socket s = new Socket(InetAddress.getLocalHost(), 9090);
-        while (true) {
-            String message = JOptionPane.showInputDialog(
-                    "Enter your message to server:");
-            PrintWriter out =
-                    new PrintWriter(s.getOutputStream(), true);
-            out.println(message);
-            if(JOptionPane.showConfirmDialog(null,
-                    "One more message?", "One question...", JOptionPane.YES_NO_OPTION) == 1) {
-            }
-            else {
-                System.exit(0);
-            }
+
+    Socket s;
+    ClientUI uInter;
+    PrintWriter out;
+
+    socketClient(ClientUI ui) {
+        uInter = ui;
+    }
+
+    public void run() {
+        try {
+            s = new Socket(InetAddress.getLocalHost(), 9090);
+            out = new PrintWriter(s.getOutputStream(), true);
+        }catch (IOException e) {
+            e.printStackTrace();
         }
+    }
+
+    void sendMessage() {
+            String msg = uInter.getMsgField().getText();
+            out.println(msg);
+    }
+
+    void sendLastMsg(){
+        System.exit(0);
     }
 }
