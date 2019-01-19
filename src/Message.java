408091389a501db7c0e41453a579fbe430ed6f9a
Andrew <avashchenock@gmail.com>
2019-01-20 01:20:25 +0200

    sending 1 object with msg

---
+++ src/Message.java
@@ -0,0 +1,11 @@
+public class Message {
+    private static String messageText;
+
+    public static String getMessageText(){
+        return messageText;
+    }
+
+    public static void setMessageText(String msg) {
+        messageText = msg;
+    }
+}
